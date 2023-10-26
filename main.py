f39c20ec3ac7f821a3ac7699d40ada06282d4e0f
arins <asareddy12345@gmail.com>
2023-10-25 20:49:05 -0400

    main

--- main.py
+++ main.py
@@ -1,4 +1,39 @@
- # Lab 6
+"""
+Lab 6
+Authors: Shan Sundal and Arin Sareddy
+Class: COP3502C
+Section: 22282
+Description: Git hub encode decode lab
+"""
 
-hclkjsDhf;lashurweo
- def main()
+
+def encoder(password_input):
+    string_result = ''
+    for i in password_input:
+        result = 0
+        result = int(i) + 3
+        string_result += str(result)
+    return string_result
+
+def decoder(password_input):
+    pass
+
+def main():
+    while True:
+        print('Menu')
+        print('-------------')
+        print('1. Encode\n2. Decode\n3. Quit')
+        user_input = int(input('Please enter an option: '))
+        if user_input == 1:
+            password_input = input('Please enter your password to encode: ')
+            print('Your passworrd has been encoded and stored!')
+            encoded_password = encoder(password_input)
+        if user_input == 2:
+            decoded_password = decoder(encoded_password)
+            print(f'The encoded password is {encoded_password}, and the original password is {decoded_password}.')
+        if user_input == 3:
+            break
+
+
+if __name__ == '__main__':
+    main()
